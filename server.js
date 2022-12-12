c5b106f5f1446bdc67c23b42d79520233cb1b9e9
Ben Binder <ben.binder@switchbackmedical.com>
2022-12-12 15:36:03 -0600

    Finish login/signup page, validate routes, adjust main.handlebars.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -15,8 +16,12 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // expires after 24 hours
+    httpOnly: true,
+    sameSite: 'strict'
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -34,7 +39,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable modular routes
 app.use(routes);
+// Provide catch-all routes
+app.get('*', (req,res) => {
+  res.status(404).render('404',{
+    logged_in: req.session.logged_in,
+    user_id: req.session.user_id,
+    username: req.session.username
+  });
+});
+app.post('*', (req,res) => {
+  res.status(405).json({message:"Route not found, or HTTP method not allowed."});
+});
+app.put('*', (req,res) => {
+  res.status(405).json({message:"Route not found, or HTTP method not allowed."});
+});
+app.delete('*', (req,res) => {
+  res.status(405).json({message:"Route not found, or HTTP method not allowed."});
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@
 // and returns a JSON object for unauthorized calls 
 // with a descriptive message.
 const withAuthAPI = (req, res, next) => {
-  if (!req.session.logged_in) {
+  if (!req.session.loggedIn) {
     res.status(403).json({message:'Route requires an authenticated login.'});
   } else {
     next();
@@ -12,8 +12,8 @@ const withAuthAPI = (req, res, next) => {
 // This middleware function is used for view routes
 // and reidrects to the login page for unauthorized calls.
 const withAuthView = (req, res, next) => {
-  if (!req.session.logged_in) {
-    res.redirect('./login');
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
   } else {
     next();
   }
