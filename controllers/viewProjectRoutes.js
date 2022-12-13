5e30f0b88e760eac3afc39a9749370679aec0395
Jeff Fleischman <FleischmanJ88@gmail.com>
2022-12-12 21:30:36 -0600

    homepage handlebars loaded

--- controllers/viewProjectRoutes.js
+++ controllers/viewProjectRoutes.js
@@ -1,28 +1,26 @@
-const router = require('express').Router();
-const { Project, User, Task } = require('../models');
-const { withAuthView } = require('../utils/auth');
-const { getNestedTasks } = require('./helpers');
+const router = require("express").Router();
+const { Project, User, Task } = require("../models");
+const { withAuthView } = require("../utils/auth");
+const { getNestedTasks } = require("./helpers");
 
 // Display single project
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     // Get the project details.
     const projectData = await Project.findByPk(req.params.id, {
-      include: [{model: User}]
+      include: [{ model: User }],
     });
-    const project = projectData.get({plain:true});
+    const project = projectData.get({ plain: true });
     // If the user is logged in, check if they have ownership
     // on this project, and set a flag for editing capability if so.
 
     // Render the page.
-    res.render('singleProject', {
+    res.render("singleProject", {
       project,
       loggedIn: req.session.loggedIn,
       username: req.session.username,
-      userID: req.session.userID
+      userID: req.session.userID,
     });
-    
-
   } catch (err) {
     res.status(500).json(`Internal Server Error: ${err.name}`);
   }
