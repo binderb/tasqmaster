86ad5a6c38697eed8d8b578185f665453cf6c6a9
Ben Binder <ben.binder11@gmail.com>
2022-12-12 19:37:14 -0600

    Add new routes for retrieving projects.

--- controllers/viewMainRoutes.js
+++ controllers/viewMainRoutes.js
@@ -10,12 +10,30 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/dashboard', withAuthView, (req, res) => {
-  res.render('dashboard', {
-    loggedIn: req.session.loggedIn,
-    username : req.session.username,
-    userID : req.session.userID
-  });
+router.get('/dashboard', withAuthView, async (req, res) => {
+  // Get a list of all projects belonging to the logged in user
+  try {
+    const allProjectsData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          where: {
+            id: req.session.id
+          },
+          required: false
+        }
+      ]
+    });
+    const allProjects = allProjectsData.map(e => e.get({plain:true}));
+    res.render('dashboard', {
+      allProjects,
+      loggedIn: req.session.loggedIn,
+      username : req.session.username,
+      userID : req.session.userID
+    });
+  } catch (err) {
+    res.status(500).json({message: `Internal Server Error: ${err.name}: ${err.message}.`});
+  }
 });
 
 router.get("/login", (req,res) => {
