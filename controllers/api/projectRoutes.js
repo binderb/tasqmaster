346e68fc6b945896dfb7c6f16cd7d3fb3e578261
Ben Binder <ben.binder11@gmail.com>
2022-12-12 18:26:11 -0600

    Implement project creation routes.

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,21 +1,49 @@
 const router = require('express').Router();
-const { Project, User, Task } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Project, User, Task, ProjectUser } = require('../../models');
+const { withAuthAPI } = require('../../utils/auth');
 const { getNestedTasks } = require('../helpers');
 
-router.get('/:id', async (req, res) => {
-  
-  const projectData = await Project.findByPk(req.params.id, {
-    include: [{model: User}]
-  });
-  const project = projectData.get({plain:true});
+// Create 1 project
+router.post('/', withAuthAPI, async (req, res) => {
+  try {
+    if (!req.body.title) {
+      res.status(403).json({message: 'Your project title cannot be empty!'});
+      return;
+    } else if (!req.body.description) {
+      res.status(403).json({message: 'Your project description cannot be empty!'});
+      return;
+    }
+    const new_project_data = await Project.create(req.body);
+    const user_list = [req.session.user_id, ...req.body.user_list];
+    const project_user_body_array = user_list.map(id => {
+      return {
+        project_id: new_project_data.id,
+        user_id: id
+      };
+    });
+    await ProjectUser.bulkCreate(project_user_body_array);
+    res.status(201).json(new_project_data);
+  } catch (err) {
+    res.status(500).json({message: `Internal Server Error: ${err.name}: ${err.message}`});
+  }
+});
 
-  const allTasks = await getNestedTasks(
-    {title:project.title, id:null}, 
-    project.id
-  );
-  // res.status(200).json(project);
-  res.status(200).json(allTasks);
+// Get 1 project (including all nested tasks)
+router.get('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [{model: User}]
+    });
+    const project = projectData.get({plain:true});
+
+    const allTasks = await getNestedTasks(
+      {title:project.title, id:null}, 
+      project.id
+    );
+    res.status(200).json(allTasks);
+  } catch (err) {
+    res.status(500).json({message: `Internal Server Error: ${err.name}: ${err.message}.`});
+  }
 
 });
 
--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -1,22 +1,8 @@
 const router = require('express').Router();
 const { Project, User, Task } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { withAuthAPI } = require('../../utils/auth');
 const { getNestedTasks } = require('../helpers');
 
-router.get('/:id', async (req, res) => {
-  
-  const projectData = await Project.findByPk(req.params.id, {
-    include: [{model: User}]
-  });
-  const project = projectData.get({plain:true});
 
-  const allTasks = await getNestedTasks(
-    {title:project.title, id:null}, 
-    project.id
-  );
-  // res.status(200).json(project);
-  res.status(200).json(allTasks);
-
-});
 
 module.exports = router;
