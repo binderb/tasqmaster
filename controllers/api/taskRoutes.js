bf017c3d095b4d894903829f1d66b4781cbbe032
William Hirsch <Williamhirschh@gmail.com>
2022-12-13 20:48:52 -0600

    added a post for creating a new task

--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -4,5 +4,32 @@ const { withAuthAPI } = require('../../utils/auth');
 const { getNestedTasks } = require('../helpers');
 
 
+//-------------------------------CREATES A NEW TASK-------------------------------//
+router.post('/', withAuthAPI, async (req, res) => {
+    try {
+      if (!req.body.title) {
+        res.status(403).json({message: 'Your task title cannot be empty!'});
+        return;
+      } else if (!req.body.description) {
+        res.status(403).json({message: 'Your task description cannot be empty!'});
+        return;
+      } else if (!req.body.userList) {
+        res.status(403).json({message: 'You must give access to this task to at least 1 user!'});
+        return;
+      }
+      const new_task_data = await Project.create(req.body);
+      const task_user_body_array = req.body.userList.map(id => {
+        return {
+          task_id: new_task_data.id,
+          user_id: id
+        };
+      });
+      await TaskUser.bulkCreate(task_user_body_array);
+      res.status(201).json(new_task_data);
+    } catch (err) {
+      res.status(500).json({message: `Internal Server Error: ${err.name}: ${err.message}`});
+    }
+  })
+  //----------------------------------------------------------------------------------------//
 
 module.exports = router;
