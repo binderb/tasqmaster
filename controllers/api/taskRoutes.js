5326c00e88ca33fe164c391a501eac5b41097583
Ben Binder <ben.binder11@gmail.com>
2022-12-13 20:44:10 -0600

    Set up get route for parent tasks in task editor.

--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -3,6 +3,25 @@ const { Project, User, Task } = require('../../models');
 const { withAuthAPI } = require('../../utils/auth');
 const { getNestedTasks } = require('../helpers');
 
+// Get an alphabetical list of tasks for 1 project
+router.get('/projects/:id', async (req, res) => {
+  try {
+    const allTasksData = await Task.findAll({
+      where: {
+        project_id: req.params.id
+      },
+      order: [['title','DESC']]
+    });
+    if (!allTasksData) {
+      res.status(404).json({message:'Project ID not found!'});
+      return;
+    }
+    const allTasks = allTasksData.map(e => e.get({plain:true}));
+    res.status(200).json(allTasks);
+  } catch (err) {
+    res.status(500).json({message: `Internal Server Error: ${err.name}: ${err.message}`});
+  }
 
+});
 
 module.exports = router;
--- public/js/manageTaskEditor.js
+++ public/js/manageTaskEditor.js
@@ -1,9 +1,23 @@
-const showEditor = (isNew) => {
+const showEditor = async (e, isNew) => {
   document.querySelector('#project-details').setAttribute('style','display:none;');
   document.querySelector('#task-details').setAttribute('style','display:none;');
   document.querySelector('#task-editor').setAttribute('style','display:block;');
   document.querySelector('#delete-task').disabled = true;
   document.querySelector('#new-task').disabled = true;
+
+  const project_id = document.querySelector('#project-title').getAttribute('data-id');
+  const allTasksResponse = await fetch('/api/tasks/projects/'+project_id);
+  const allTasks = await allTasksResponse.json();
+
 }
 
-document.querySelector('#new-task').addEventListener('click', () => {showEditor(true)});
+const hideEditor = (e) => {
+  document.querySelector('#project-details').setAttribute('style','display:block;');
+  document.querySelector('#task-details').setAttribute('style','display:none;');
+  document.querySelector('#task-editor').setAttribute('style','display:none;');
+  document.querySelector('#delete-task').disabled = true;
+  document.querySelector('#new-task').disabled = false;
+}
+
+document.querySelector('#new-task').addEventListener('click', (e) => {showEditor(e,true)});
+document.querySelector('#cancel-new-task').addEventListener('click', hideEditor);
